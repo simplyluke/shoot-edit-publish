b84747512ad711b2db0eeb45e10b76bf324545a7
Luke Wright <lukewright1997@gmail.com>
2013-12-28 17:31:14 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shooteditpublish::Application.config.secret_key_base = '1d99b210759d951c59661c7c2d9abf399c88f737fb3a83285f121b7a443a46388e82e84943ef9e24ec00d8da81766dab94585eb3487cb760500d799743741234'
