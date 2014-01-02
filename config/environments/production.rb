9c975056bd8e1d3b48038dae6ba93a551ce75b5a
Luke Wright <lukewright1997@gmail.com>
2014-01-02 00:18:51 -0600

    may have been this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Shooteditpublish::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.default_url_options = { :host => 'localhost:3000'} 
+  config.action_mailer.default_url_options = { :host => 'https://shielded-caverns-7458.herokuapp.com'} 
   config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
